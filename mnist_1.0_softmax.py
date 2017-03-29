f6d502f7cbd779b4b8a8587e4222a342a615949d
Agnivah <agnivahp@gmail.com>
2017-03-29 09:56:01 -0500

    changes

--- mnist_1.0_softmax.py
+++ mnist_1.0_softmax.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#apapapapapapap
 import tensorflow as tf
 import tensorflowvisu
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
